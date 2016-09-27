f837d16feaa2ec08c30c9b209b6ccf9b515d53fd
Poly <0122Haruo@gmail.com>
2016-09-27 20:38:14 +0900

    update

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ var cHeight = 640;
 var whrate = cWidth / cHeight;
 var images = {};
 var time = 0;
-var delta = 0;
+var fps = 60;
+var delta = 1/fps;
 // 0:title, 1:play, 2:gameover, 3:clear
 var scene = 0;
 var gdr = 0;
@@ -71,6 +72,15 @@ var mzut = (function() {
     },
     damage : function() {
       hp--;
+    },
+    setX : function(x1) {
+      x = x1;
+    },
+    getSize : function() {
+      return size;
+    },
+    getV : function() {
+      return vx;
     }
   }
 })();
@@ -95,7 +105,7 @@ var honkan = (function() {
       height = width / 5 * 4;
       dx = 0;
       dy = 0;
-      hp = 120;
+      hp = 1200;
       destrotime = 0;
       destroyed = false;
     },
@@ -136,20 +146,19 @@ var honkan = (function() {
   }
 })();
 
-var foccatio = function(x1, y1, vx1, vy1){
-  var x = x1;
-  var y = y1;
-  var vx = vx1;
-  var vy = vy1;
+var foccatio = function(f){
+  var x = f(0).x;
+  var y = f(0).y;
+  var t = 0;
   var r = 12;
   var dead = false;
   var explode = false;
   var explodetime = 0;
   var rotate = Math.random() * 10;
-  var collision = function() {
+  var collision = function(x, y, r) {
     var dist = (x-mzut.shape().x)*(x-mzut.shape().x) + (y-mzut.shape().y)*(y-mzut.shape().y);
-    return (dist < (r+mzut.shape().size)*(r+mzut.shape().size));
-  };
+    return (dist < (r+mzut.shape().size*0.5)*(r+mzut.shape().size*0.5));
+  }
   return {
     draw : function(c) {
       if(explodetime < 0.01){
@@ -161,11 +170,12 @@ var foccatio = function(x1, y1, vx1, vy1){
       }
     },
     update : function() {
+      t += delta;
       if(!explode) {
-        x += vx;
-        y += vy;
+        x = f(t).x;
+        y = f(t).y;
       }
-      if(y < 30 && !explode) {
+      if(y < fo.ylimit && !explode) {
         explode = true;
         honkan.setExtime();
         exps.add(x, y, r, r*2/3, 0.6, 1);
@@ -176,7 +186,7 @@ var foccatio = function(x1, y1, vx1, vy1){
       if(explodetime > 0.6) {
         dead = true;
       }
-      if(collision() && !explode) {
+      if(collision(x, y, r) && !explode) {
         mzut.damage();
       }
     },
@@ -186,16 +196,122 @@ var foccatio = function(x1, y1, vx1, vy1){
     },
     getDead : function() {
       return dead;
-    }
+    },
+    collision : collision
   }
 }
 
 var fo = (function() {
-  var list = [];
-  var spawnwait = 0.3;
+  var list;
+  var spawnwait;
+  var mzutorbit;
+  var t;
+  var dmzut;
+  var mzutwait
+  function fall(x, tlimit) {
+    return function(t) {
+      return {
+        x : x,
+        y : 390 + (fo.ylimit - 390) * t / tlimit
+      }
+    }
+  }
+  function line(x, tlimit, x1) {
+    var v = Math.sqrt((x-x1)*(x-x1)+360*360)/tlimit;
+    console.log(v);
+    return function(t) {
+      return {
+        x : x + (x1 - x) * t / tlimit,
+        y : 390 + (fo.ylimit - 390) * t / tlimit
+      }
+    }
+  }
+  function bezier2(x, tlimit, x1, y1, x2) {
+    return function(t) {
+      var t1 = t / tlimit;
+      return {
+        x : (1-t1)*(1-t1)*x + 2*(1-t1)*t1*x1 + t1*t1*x2,
+        y : (1-t1)*(1-t1)*390 + 2*(1-t1)*t1*y1 + t1*t1*fo.ylimit
+      }
+    }
+  }
+  function bezier(x, tlimit, x1, y1, x2, y2, x3) {
+    return function(t) {
+      var t1 = t / tlimit;
+      return {
+        x : (1-t1)*(1-t1)*(1-t1)*x + 3*(1-t1)*(1-t1)*t1*x1 + 3*(1-t1)*t1*t1*x2 + t1*t1*t1*x3,
+        y : (1-t1)*(1-t1)*(1-t1)*390 + 3*(1-t1)*(1-t1)*t1*y1 + 3*(1-t1)*t1*t1*y2 + t1*t1*t1*fo.ylimit
+      }
+    }
+  }
+
   return {
-    generate : function(x, y, vx, vy) {
-      list.push(foccatio(x, y, vx, vy));
+    ylimit : 30,
+    init : function() {
+      list = [];
+      spawnwait = 0.3;
+      mzutorbit = [0];
+      dmzut = 0;
+      mzutwait = 0;
+      for(var i = 1; i < fps*16; i++) {
+        if(mzutwait < 0) {
+          dmzut = mzut.getV() * (Math.floor(Math.random()*3)-1) * 0.5;
+          mzutwait = Math.random()*0.2+0.1;
+        }
+        mzutwait -= delta;
+        if(mzutorbit[i-1] + dmzut < -100+mzut.getSize()/2 || mzutorbit[i-1] + dmzut > 100-mzut.getSize()/2) {
+          dmzut = -dmzut;
+        }
+        mzutorbit[i] = mzutorbit[i-1] + dmzut;
+      }
+      t = 0;
+    },
+    generate : function(tlimit) {
+      // tlimit = Math.random() * Math.random() * 15.4 + 0.6;
+      var flag = 0;
+      if(Math.random() < 0.2){
+        flag = 0;
+      } else if(Math.random() < 0.5) {
+        flag = 1;
+      } else if(Math.random() < 0.4) {
+        flag = 2;
+      } else {
+        flag = 3;
+      }
+      var max = -100000000;
+      var min = 100000000;
+      var po = 0.8;
+      for(var i = Math.floor(tlimit * po * fps); i < tlimit * fps; i++) {
+        max = Math.max(max, mzutorbit[i]);
+        min = Math.min(min, mzutorbit[i]);
+      }
+      max += 26;
+      min -= 26;
+      var chakudan;
+      var spawnx;
+      if(Math.random() < 0.5) {
+        chakudan = Math.random()*(min+120)-120;
+        spawnx = Math.random()*(min+120)-120;
+      } else {
+        chakudan = Math.random()*(120-max)+max;
+        spawnx = Math.random()*(120-max)+max;
+      }
+      // var chakudan = Math.random()*(240-(max-min))-120;
+      // var spawnx = Math.random()*(120+min) - 120;
+      // if(chakudan > min) {
+      //   chakudan += max - min;
+      //   spawnx = Math.random()*(120-max)+max;
+      // }
+      if(flag == 0) {
+        list.push(foccatio(fall(chakudan, tlimit)));
+      } else if(flag == 1) {
+        var dsx = spawnx - chakudan;
+        list.push(foccatio(line(chakudan + dsx * 1/(1-po), tlimit, chakudan)));
+      } if(flag == 2) {
+        list.push(foccatio(bezier2(Math.random()*240-120, tlimit, spawnx, 100+Math.random()*20, chakudan)));
+      } else {
+        list.push(foccatio(bezier(Math.random()*240-120, tlimit, Math.random()*240-120, Math.random()*180+100, spawnx, 100+Math.random()*20, chakudan)));
+      }
     },
     destroy : function() {
       list.forEach(function(value){
@@ -208,10 +324,14 @@ var fo = (function() {
       });
     },
     update : function(){
+      // mzut.setX(mzutorbit[0]);
+      t += delta;
       spawnwait -= delta;
       if(spawnwait < 0) {
-        fo.generate(Math.random()*400-200, 600, Math.random() - 0.5, -0.2 - Math.random()*4);
-        spawnwait = Math.random() * 0.25 + 0.12
+        // fo.generate(Math.random()*400-200, 600, Math.random() - 0.5, -0.2 - Math.random()*4);
+        var tlimit = Math.random() * 15.4 + 0.6;
+        fo.generate(tlimit);
+        spawnwait = (Math.random() * 0.25 + 0.12)/16*tlimit;
       }
 
       var newList = [];
@@ -222,6 +342,18 @@ var fo = (function() {
         }
       });
       list = newList;
+      mzutwait -= delta;
+      for(var i = 1; i < fps*16; i++) {
+        mzutorbit[i-1] = mzutorbit[i];
+      }
+      if(mzutwait < 0) {
+        dmzut = mzut.getV() * (Math.floor(Math.random()*3)-1) * 0.5;
+        mzutwait = Math.random()*0.2+0.1;
+      }
+      if(mzutorbit[fps*16-2] + dmzut < -100+mzut.getSize()/2 || mzutorbit[fps*16-2] + dmzut > 100-mzut.getSize()/2) {
+        dmzut = -dmzut;
+      }
+      mzutorbit[fps*16-1] = mzutorbit[fps*16-1] + dmzut;
     }
   }
 })();
@@ -338,6 +470,7 @@ function setting() {
 function init() {
   honkan.init();
   mzut.init();
+  fo.init();
 }
 
 function update(timestamp) {
